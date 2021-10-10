61c4776ec36896fd22205a49c2823803c927fb5b
jan-zelaznog <jan.zelaznog@gmail.com>
2021-10-10 16:58:53 -0500

    avance proyecto final
    Estado del proyecto final después del postwork de la sesión 4  - módulo 1

---
+++ Módulo-01/Sesión-04/MyApp/MyApp/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Ricardo Montesinos on 08/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func `continue`(_ sender: Any) {
+        let name: String = #function
+        print (name)
+    }
+    
+    @IBAction func signup(_ sender: Any) {
+        let name: String = #function
+        print (name)
+    }
+    
+    override func viewDidLoad() {
+    super.viewDidLoad()
+      let name: String = #function
+      print (name)
+  }
+
+    override func viewWillLayoutSubviews() {
+        let name: String = #function
+        print (name)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let name: String = #function
+        print (name)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let name: String = #function
+        print (name)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        let name: String = #function
+        print (name)
+    }
+}
+
